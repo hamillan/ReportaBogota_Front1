822d663d182e0a1d0ed610685182ed52f6483506
Unknown <mdazag@gmail.com>
2018-09-22 09:44:47 -0500

    Adición página camara

---
+++ src/pages/camara/camara.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
+/**
+ * Generated class for the CamaraPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-camara',
+  templateUrl: 'camara.html',
+})
+export class CamaraPage {
+  public base64Image: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private camera: Camera) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CamaraPage');
+  }
+
+  takePicture(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     this.base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     console.log(err);
+    });
+  }
+
+}
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -7,6 +7,6 @@ export const MainPage = 'TabsPage';
 
 // The initial root pages for our tabs (remove if not using tabs)
 export const Tab1Root = 'LandingPage';
-export const Tab2Root = 'SearchPage';
+export const Tab2Root = 'CamaraPage';
 export const Tab3Root = 'SettingsPage';
 export const Tab4Root = 'LandingPage';
